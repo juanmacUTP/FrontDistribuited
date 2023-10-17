c23663bd6c07defd1c6cec90c4441465ac76c4ed
juanmacUTP <juan.corrales@utp.edu.co>
2023-10-17 09:52:57 -0500

    [Fix]: some fixes

---
+++ Conecction/sign_up.js
@@ -0,0 +1,9 @@
+fetch("http://146.190.117.195:5000/api/signup", {
+    method: "POST",
+    body:
+        {
+            "name": "Juan Ma",
+            "email": "chupelo@gmail.com",
+            "password": "miguelesgay"
+        }
+})
